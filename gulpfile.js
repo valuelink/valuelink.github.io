2ec972f34d27f36b42ef95cb4bd727edbd7cc9a3
wangkun23 <wang.kun@jinyinwu.com>
2017-06-12 14:32:27 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,31 @@
 //导入工具包 require('node_modules里对应模块')
-var gulp = require('gulp'), //本地安装gulp所用到的地方
-	livereload = require('gulp-livereload');
-    less = require('gulp-less');
+var gulp = require('gulp');//本地安装gulp所用到的地方
+var browserSync = require('browser-sync').create();
+var less = require('gulp-less');
 
-//定义一个testLess任务（自定义任务名称）
-gulp.task('testLess', function () {
+//定义一个lessTask任务（自定义任务名称）
+gulp.task('lessTask', function () {
     gulp.src('src/less/app.less') //该任务针对的文件
         .pipe(less()) //该任务调用的模块
-        .pipe(gulp.dest('src/css')); //将会在src/css下生成index.css
+        .pipe(gulp.dest('src/css'))
+        .pipe(browsersync.stream()); //将会在src/css下生成app.css
 });
 
-gulp.task('default',['testLess']); //定义默认任务
-
-gulp.task('testWatch', function () {
-    gulp.watch('src/**/*.less', ['testLess']); //当所有less文件发生改变时，调用testLess任务
-});
-
-gulp.task('watch', function () {    // 这里的watch，是自定义的，写成live或者别的也行
-    var server = livereload();
-    
-    // app/**/*.*的意思是 app文件夹下的 任何文件夹 的 任何文件
-    gulp.watch('src/*.html', function (file) {
-        server.changed(file.path);
+gulp.task('serve', function() {
+    browserSync.init({
+        port: 80,
+        server: {
+            baseDir: ['src']
+        }
     });
+    gulp.watch('src/**/*.less', ['lessTask']);
+
+    gulp.watch("./src/css/*.css").on('change', browserSync.reload);  
+    gulp.watch("./src/*.html").on('change', browserSync.reload);  
+    gulp.watch("./src/js/*.js").on('change', browserSync.reload);  
 });
+
+gulp.task('default',['serve']); //定义默认任务
 
 //gulp.task(name[, deps], fn) 定义任务  name：任务名称 deps：依赖任务名称 fn：回调函数
 //gulp.src(globs[, options]) 执行任务处理的文件  globs：处理的文件路径(字符串或者字符串数组) 
